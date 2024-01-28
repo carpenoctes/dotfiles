839d77ef40b49ffa0468e5106af9d64581a5cf8a
carpenoctes <saddaf.shabbir@gmail.com>
2024-01-28 22:48:26 +0100

    backup

--- scripts/scripts/dunst/clippy.sh
+++ scripts/scripts/dunst/clippy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 grim -g "$(slurp)" '/home/saddaf/scripts/dunst/grimtemp.png'
-#wl-copy < "/home/saddaf/scripts/dunst/grimtemp.png" doesn't work anymore after reinstalling arch
-xclip -in -selection clipboard -target image/png /home/saddaf/scripts/dunst/grimtemp.png
+wl-copy < "/home/saddaf/scripts/dunst/grimtemp.png"
+#xclip -in -selection clipboard -target image/png /home/saddaf/scripts/dunst/grimtemp.png
